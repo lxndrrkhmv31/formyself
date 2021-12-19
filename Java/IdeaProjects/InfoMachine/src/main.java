4838b698dbc75a438086380e826ff08f8358e66a
lxndrrkhmv31 <catdog.puga24@gmail.com>
2021-12-19 18:31:42 +0300

    InfoAboutMachine

---
+++ Java/IdeaProjects/InfoMachine/src/main.java
@@ -0,0 +1,11 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Информация о системе");
+        System.out.print("Операционная система: ");
+        System.out.println(System.getProperty("os.name") + " " + System.getProperty("os.version"));
+        System.out.print("Количество ядер процессора: ");
+        System.out.println(Runtime.getRuntime().availableProcessors());
+        System.out.print("Количество памяти, доступной JVM: ");
+        System.out.println(Runtime.getRuntime().maxMemory() / 1024 / 1024 + "Mb");
+    }
+}
